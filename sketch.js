a8aabd24e55b1c1871bee62cf37bb0d5c26788c4
alwinhempel <alwin.hempel@gmx.net>
2026-06-02 14:05:48 +0200

    neuemethode

--- sketch.js
+++ sketch.js
@@ -1,13 +1,17 @@
-let linelength = 1600;
-let verticalgap = 100;
-let margin = 100;
-
-let steps = 24;
-let patternLength = 5;
-let duration = 1;
-let rowPoint = 0;
+let measureLength = 1600;
 let pulse = 4;
-let y = margin;
+
+let verticalgap = 100;
+let margin = 50;
+
+let value = 8;
+let modifier = 1.5;
+let noteLength = measureLength / value * modifier;
+let patternLength = 6;
+
+let circX = margin;
+let circY = margin;
+let circleradius;
 
 function setup() {
   createCanvas(2100, 2970);
@@ -17,34 +21,85 @@ function setup() {
 
   //vertical grid
   for (let j = margin; j < 2970; j += verticalgap) {
-    line(margin, j, linelength + margin, j);
+    line(margin, j, measureLength + margin, j);
   }
   
   //horizontal grid
-  for (let k = margin; k <= linelength; k += margin * pulse){
-    line(k, margin, k, linelength + margin);
+  for (let k = margin; k <= measureLength + margin; k += measureLength/pulse){
+    line(k, margin, k, 2970 + margin);
   }
 
+  for (let i = 0; i < 64; i++){
+
+    if(i % patternLength == 0){
+      circleradius = 60;
+    } else {
+      circleradius = 30;
+    };
+
   
-  //draw points
-  for (let i = 0; i < 256; i++) {
-  if (rowPoint < steps) {
-        if (i % patternLength == 0) {
-          circle(margin + rowPoint * linelength/steps, y, 50);
-          rowPoint++;
-        } else {
-          circle(margin + rowPoint * linelength/steps, y, 25);
-          rowPoint++;
-        }
-      } else {
-        y += verticalgap;
-        rowPoint = 0;
-        if (i % patternLength == 0) {
-          break;
-        } else {
-          circle(margin + rowPoint * linelength/steps, y, 25);
-          rowPoint++;
-        }
-      }
-    }
+    if (circX >= measureLength + margin){ 
+      circX -= measureLength;
+      circY += verticalgap;
+      circle(circX, circY, circleradius);
+      circX += noteLength;
+    } else {
+    circle(circX, circY, circleradius);
+    circX += noteLength;
+    };
+  
+  }
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+  // //draw points
+  // for (let i = 0; i < 256; i++) {
+  // if (rowPoint < steps) {
+  //       if (i % patternLength == 0) {
+  //         circle(margin + rowPoint * measureLength/steps, y, 50);
+  //         rowPoint++;
+  //       } else {
+  //         circle(margin + rowPoint * measureLength/steps, y, 25);
+  //         rowPoint++;
+  //       }
+  //     } else {
+  //       y += verticalgap;
+  //       rowPoint = 0;
+  //       if (i % patternLength == 0) {
+  //         break;
+  //       } else {
+  //         circle(margin + rowPoint * measureLength/steps, y, 25);
+  //         rowPoint++;
+  //       }
+  //     }
+  //   }
 }
