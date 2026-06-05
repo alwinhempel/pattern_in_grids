f505feb72e46de3ca23601e032fd097938c07375
alwinhempel <alwin.hempel@gmx.net>
2026-06-05 14:32:02 +0200

    v1

--- sketch.js
+++ sketch.js
@@ -20,25 +20,11 @@ let pulseSelect;
 function setup() {
   createCanvas(windowWidth, windowHeight*0.9);
 
-  //modifier
-  modifierSelect = createRadio();
-  modifierSelect.option(1, 'normal');
-  modifierSelect.option(1.5, 'punktiert');
-  modifierSelect.option(0.6667, 'triole');
-  modifierSelect.selected('1.5');
-  modifierSelect.changed(drawGrid);
-
-  //value
-  valueSelect = createRadio();
-  valueSelect.option(2, '1/2');
-  valueSelect.option(4, '1/4');
-  valueSelect.option(8, '1/8');
-  valueSelect.option(16, '1/16');
-  valueSelect.selected('16')
-  valueSelect.changed(drawGrid);
 
   //pattenLength
   patternLengthSelect = createRadio();
+  patternLengthSelect.size(300, 50);
+  patternLengthSelect.position(0,0);
   patternLengthSelect.option(1, '1');
   patternLengthSelect.option(2, '2');
   patternLengthSelect.option(3, '3');
@@ -50,8 +36,33 @@ function setup() {
   patternLengthSelect.selected('6');
   patternLengthSelect.changed(drawGrid);
 
+  //value
+  valueSelect = createRadio();
+  valueSelect.size(300, 50);
+  valueSelect.position(windowWidth/2-110, 0);
+  valueSelect.option(2, '1/2');
+  valueSelect.option(4, '1/4');
+  valueSelect.option(8, '1/8');
+  valueSelect.option(16, '1/16');
+  valueSelect.selected('16')
+  valueSelect.changed(drawGrid);
+
+  //modifier
+  modifierSelect = createRadio();
+  modifierSelect.size(300,50);
+  modifierSelect.position(windowWidth-225, 0);
+  modifierSelect.option(1, 'normal');
+  modifierSelect.option(1.5, 'punktiert');
+  modifierSelect.option(0.6667, 'triole');
+  modifierSelect.selected('1.5');
+  modifierSelect.changed(drawGrid);
+
+
+
   //Pulse
   pulseSelect = createRadio();
+  pulseSelect.size(300, 50);
+  pulseSelect.position(windowWidth/2-110, windowHeight - 50);
   pulseSelect.option(2, '1/2');
   pulseSelect.option(4, '1/4');
   pulseSelect.option(8, '1/8');
@@ -101,9 +112,9 @@ function drawGrid(){
 
     // Pattern durch Größe
     if(i % patternLength == 0){
-      circleradius = 60;
+      circleradius = 0.04 * windowHeight;
     } else {
-      circleradius = 30;
+      circleradius = 0.02 * windowHeight;
     };
 
 
